900d4149ef6908f699900a314e1ec1acb7d7fc53
Yuta Hayashibe <yuta@hayashibe.jp>
2020-05-28 13:22:30 +0900

    Added a translate menu

--- js/background.js
+++ js/background.js
@@ -3,11 +3,23 @@ function execPreprocessingCopy(info){
 	execCopy(selectiontext);
 }
 
+function openDeepL(info) {
+	const selectiontext = info.selectionText.toString();
+	window.open('https://www.deepl.com/translator#auto/ja/' + encodeURI(selectiontext));
+}
+
 chrome.contextMenus.create({
 	'title': '翻訳前処理をしてコピー',
 	'type': 'normal',
 	'contexts': ['selection'],
 	'onclick': execPreprocessingCopy,
+});
+
+chrome.contextMenus.create({
+	'title': '翻訳前処理をしてDeepL翻訳',
+	'type': 'normal',
+	'contexts': ['selection'],
+	'onclick': openDeepL,
 });
 
 console.log('Background activate');
