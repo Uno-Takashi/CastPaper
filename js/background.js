8d8864d9583edf0951bfaa2bc5a886342eb82644
otyamura <suzuki08suzuki@gmail.com>
2022-12-01 11:22:15 +0900

    add id

--- js/background.js
+++ js/background.js
@@ -1,25 +1,27 @@
-function execPreprocessingCopy(info){
-	const selectiontext=info.selectionText.toString();
-	execCopy(selectiontext);
+function execPreprocessingCopy(info) {
+  const selectiontext = info.selectionText.toString();
+  execCopy(selectiontext);
 }
 
 function openDeepL(info) {
-	const selectiontext = info.selectionText.toString();
-	window.open('https://www.deepl.com/translator#auto/ja/' + encodeURI(selectiontext));
+  const selectiontext = info.selectionText.toString();
+  window.open('https://www.deepl.com/translator#auto/ja/' + encodeURI(selectiontext));
 }
 
 chrome.contextMenus.create({
-	'title': '翻訳前処理をしてコピー',
-	'type': 'normal',
-	'contexts': ['selection'],
-	'onclick': execPreprocessingCopy,
+  'id': 'copy',
+  'title': '翻訳前処理をしてコピー',
+  'type': 'normal',
+  'contexts': ['selection'],
+  'onclick': execPreprocessingCopy,
 });
 
 chrome.contextMenus.create({
-	'title': '翻訳前処理をしてDeepL翻訳',
-	'type': 'normal',
-	'contexts': ['selection'],
-	'onclick': openDeepL,
+  'id': 'translate',
+  'title': '翻訳前処理をしてDeepL翻訳',
+  'type': 'normal',
+  'contexts': ['selection'],
+  'onclick': openDeepL,
 });
 
 console.log('Background activate');
